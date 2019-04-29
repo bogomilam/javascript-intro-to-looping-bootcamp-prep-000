8b581188925572a1b0b1d69d77e8018027b6033b
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-29 22:20:30 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,5 +17,12 @@ return "done"
 }
 
 function doWhileLoop(integer){
-  
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < 5);
 }
